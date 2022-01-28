f0a236650c8151a07d16332e6b50981436280fa6
adachi <316840383@qq.com>
2022-01-28 15:57:08 +0800

    fix bugs

--- extractors/local_comm.py
+++ extractors/local_comm.py
@@ -46,17 +46,6 @@ class LocalCommunityExtractor(BaseExtractor):
                     tx = {header[i]: row[i] for i in range(len(header))}
                     g.add_edge(tx.get('from'), tx.get('to'))
 
-            # load txs
-            # txs = list()
-            # with open(in_txs_fn, 'r', encoding='utf-8') as f:
-            #     reader = csv.reader(f)
-            #     header = next(reader)
-            #     out_writer.writerow(header)
-            #
-            #     for row in reader:
-            #         tx = {header[i]: row[i] for i in range(len(header))}
-            #         txs.append(tx)
-
             # load importance
             p = dict()
             in_importance_fn = os.path.join(self.args.in_dir, 'importance', fn)
@@ -68,7 +57,7 @@ class LocalCommunityExtractor(BaseExtractor):
 
             # get local community nodes
             source = fn.split('.')[0]
-            local_comm_nodes = self._local_comm_v2(source, g, p)
+            local_comm_nodes = self._local_comm(source, g, p)
 
             # write tx for output
             out_file = open(out_txs_fn, 'w', newline='', encoding='utf-8')
@@ -83,11 +72,8 @@ class LocalCommunityExtractor(BaseExtractor):
                     if tx.get('from') in local_comm_nodes and tx.get('to') in local_comm_nodes:
                         out_writer.writerow(row)
             out_file.close()
-            # for tx in self._local_comm(source, txs, p):
-            #     out_writer.writerow([tx[h] for h in header])
-            # out_file.close()
 
-    def _local_comm_v2(self, source, g: nx.Graph, p: dict) -> set:
+    def _local_comm(self, source, g: nx.Graph, p: dict) -> set:
         inter_nodes = {source}
         outer_nodes = set()
         inter_sum, outer_sum = 0, 0
@@ -124,54 +110,3 @@ class LocalCommunityExtractor(BaseExtractor):
                 inter_sum, outer_sum, new_node, g, inter_nodes, outer_nodes, p
             )
         return inter_nodes
-
-    def _local_comm(self, source, txs: list, p: dict) -> list:
-        g = nx.MultiDiGraph()
-        for tx in txs:
-            g.add_edge(tx['from'], tx['to'], id=tx['id'])
-
-        def _calc_conductance_incr(inter_sum, outer_sum, new_node, g, inter_nodes, outer_nodes, p):
-            inter_nodes.add(new_node)
-            inter_sum += p[new_node]
-
-            if new_node in outer_nodes:
-                outer_sum -= p.get(new_node, 0)
-
-            for e in g.in_edges(new_node):
-                if e[0] not in inter_nodes and e[0] not in outer_nodes:
-                    outer_nodes.add(e[0])
-                    outer_sum += p.get(e[0], 0)
-            for e in g.out_edges(new_node):
-                if e[1] not in inter_nodes and e[1] not in outer_nodes:
-                    outer_nodes.add(e[1])
-                    outer_sum += p.get(e[1], 0)
-            return inter_sum, outer_sum, inter_nodes, outer_nodes
-
-        inter_nodes = {source}
-        outer_nodes = set()
-        inter_sum, outer_sum = 0, 0
-
-        p_items = list()
-        p_sum = 0
-        for k, v in p.items():
-            p_sum += v
-            if k != source:
-                p_items.append((k, v))
-        p_items = [(item[0], item[1] / p_sum) for item in p_items]
-        p_items.sort(key=lambda x: x[1])
-
-        inter_sum, outer_sum, inter_nodes, outer_nodes = _calc_conductance_incr(
-            inter_sum, outer_sum, source, g, inter_nodes, outer_nodes, p
-        )
-        while outer_sum >= self.args.phi * (inter_sum + outer_sum) and len(p_items) > 0:
-            new_node, weight = p_items.pop()
-            inter_sum, outer_sum, inter_nodes, outer_nodes = _calc_conductance_incr(
-                inter_sum, outer_sum, new_node, g, inter_nodes, outer_nodes, p
-            )
-
-        _txs = list()
-        _txs_hash = set([attr['id'] for _, _, attr in g.subgraph(inter_nodes).edges(data=True)])
-        for tx in txs:
-            if tx['id'] in _txs_hash:
-                _txs.append(tx)
-        return _txs
