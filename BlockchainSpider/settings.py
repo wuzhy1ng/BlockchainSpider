6d8482724d128bce46b66c284e41a73c3c4ef2c8
wuzhy95 <870167019@qq.com>
2024-03-02 16:23:41 +0800

    update settings.py

--- BlockchainSpider/settings.py
+++ BlockchainSpider/settings.py
@@ -64,10 +64,10 @@ DOWNLOADER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
     # 'contrib.mots.pipelines.MoTSPipeline': 666,
     # 'contrib.rabbit.pipelines.RabbitMQPipeline': 666,
-}
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
