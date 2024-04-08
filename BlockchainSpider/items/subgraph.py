ab00e52ebf909572c01385c3804e22ba27481f0b
wuzhy95 <870167019@qq.com>
2024-04-08 14:48:00 +0800

    update txs spiders (fix bug)

--- BlockchainSpider/items/subgraph.py
+++ BlockchainSpider/items/subgraph.py
@@ -2,12 +2,12 @@ import scrapy
 
 
 class SubgraphTxItem(scrapy.Item):
-    source = scrapy.Field()
-    tx = scrapy.Field()
+    source = scrapy.Field()  # str
+    tx = scrapy.Field()  # Dict
     task_info = scrapy.Field()
 
 
 class ImportanceItem(scrapy.Item):
-    source = scrapy.Field()
-    importance = scrapy.Field()
+    source = scrapy.Field()  # str
+    importance = scrapy.Field()  # Dict[str, float]
     task_info = scrapy.Field()
