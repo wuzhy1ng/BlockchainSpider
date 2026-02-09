9c60f22ec1962c20c0c0c606db0d0683a9d170d2
wuzhy95 <870167019@qq.com>
2026-02-09 14:32:13 +0800

    fix bugs of label pipeline

--- BlockchainSpider/pipelines/label.py
+++ BlockchainSpider/pipelines/label.py
@@ -7,18 +7,21 @@ from BlockchainSpider.items.label import LabelReportItem
 class LabelReportPipeline:
     def __init__(self):
         self.file = None
+        self.out_dir = './data'
+
+    def open_spider(self, spider):
+        if getattr(spider, 'out_dir'):
+            self.out_dir = spider.out_dir
+        if not os.path.exists(self.out_dir):
+            os.makedirs(self.out_dir)
 
     def process_item(self, item, spider):
-        if spider.out_dir is None:
-            return item
         if not isinstance(item, LabelReportItem):
             return item
 
         # init file from filename
         if self.file is None:
-            fn = os.path.join(spider.out_dir, LabelReportItem.__name__)
-            if not os.path.exists(spider.out_dir):
-                os.makedirs(spider.out_dir)
+            fn = os.path.join(self.out_dir, LabelReportItem.__name__)
             self.file = open(fn, 'a')
 
         # write item
